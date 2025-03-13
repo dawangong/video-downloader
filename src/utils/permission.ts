dfc5408f074e0ca82a9c3976aa33ed3cd8620b65
dawangong <dawangong@126.com>
2025-03-14 00:41:49 +0800

    feat: remove

--- src/utils/permission.ts
+++ src/utils/permission.ts
@@ -1,10 +1,13 @@
-import { Platform, Linking } from 'react-native';
+import {
+  Platform,
+  //  Linking
+} from 'react-native';
 import { request, check, PERMISSIONS, RESULTS } from 'react-native-permissions';
 
 // 引导用户前往应用设置页面
-const openAppSettings = async () => {
-  await Linking.openSettings();
-};
+// const openAppSettings = async () => {
+//   await Linking.openSettings();
+// };
 
 // 存储权限
 export const requestStoragePermission = async () => {
@@ -21,8 +24,6 @@ export const requestStoragePermission = async () => {
       return true;
     } else if (status === RESULTS.BLOCKED) {
       console.log('存储权限被拒绝，且用户选择不再询问');
-      // 引导用户手动开启权限
-      // openAppSettings();
       return false;
     } else {
       console.log('存储权限未授予，开始申请权限');
@@ -33,7 +34,7 @@ export const requestStoragePermission = async () => {
       } else {
         console.log('存储权限被拒绝');
         // 引导用户手动开启权限
-        openAppSettings();
+        // openAppSettings();
         return false;
       }
     }
